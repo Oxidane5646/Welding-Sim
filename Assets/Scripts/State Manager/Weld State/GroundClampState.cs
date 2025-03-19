8fd77bf5dfac54d1a5a4feb513e5810fb059bbdd
Yathesh5646 <yathesh5646@gmail.com>
2025-03-19 15:20:12 +0530

    Update GroundClampState.cs

--- Assets/Scripts/State Manager/Weld State/GroundClampState.cs
+++ Assets/Scripts/State Manager/Weld State/GroundClampState.cs
@@ -1,26 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Transformers;
 
 public class GroundClampState : BaseWeldState
 {
+    private GameObject GroundClampSocket;
+    private GameObject GroundClamp;
+
+    private XRSocketInteractor socketInteractor;
+
+    private bool clampInserted;
+    
     public GroundClampState(GameObject panel) : base(panel) { }
 
     public override void EnterState(WeldStateManager stateManager)
     {
-        
+        panel.SetActive(true);
+
+        socketInteractor = GroundClampSocket.GetComponent<XRSocketInteractor>();
+        socketInteractor.selectEntered.AddListener(OnSelectEntered);
+        socketInteractor.selectExited.AddListener(OnSelectExited);
     }
 
     public override void ExitState(WeldStateManager stateManager)
     {
-        
+        panel.SetActive(false);
+
+        socketInteractor.selectEntered.RemoveListener(OnSelectEntered);
+        socketInteractor.selectExited.RemoveListener(OnSelectExited);
+
     }
 
     public override void UpdateState(WeldStateManager stateManager)
     {
-        if (stateManager != null)
+        
+        
+    }
+
+    private void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        if (args.interactorObject.transform.gameObject == GroundClamp)
         {
-            
+            clampInserted = true;
+        }
+        
+    }
+
+    private void OnSelectExited(SelectExitEventArgs args)
+    {
+        if (args.interactorObject.transform.gameObject == GroundClamp)
+        {
+            clampInserted = false;
         }
     }
 }
